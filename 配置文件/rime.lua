76a4662afa42c37faf70cca53a3edcfb40975daa
masonme <42291758+masonme@users.noreply.github.com>
2021-02-23 03:40:08 +0800

    去掉美式日期格式，去掉时间包含日期

--- 配置文件/rime.lua
+++ 配置文件/rime.lua
@@ -4,13 +4,11 @@ function date_translator(input, seg)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
-        yield(Candidate("date", seg.start, seg._end, os.date("%m-%d-%Y"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
     end
     if (input == "sj") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
-        yield(Candidate("time", seg.start, seg._end, os.date("%Y%m%d%H%M%S"), ""))
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
     end
 
