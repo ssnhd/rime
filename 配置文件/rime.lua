d992e3e6982c0562c4c525cfe1f9738b9e4e6c56
masonme <42291758+maomiui@users.noreply.github.com>
2021-04-19 00:44:24 +0800

    动态时间日期星期

--- 配置文件/rime.lua
+++ 配置文件/rime.lua
@@ -1,12 +1,12 @@
 function date_translator(input, seg)
-    if (input == "rq") then
+    if (input == "date") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%m-%d"), ""))
         yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
     end
-    if (input == "sj") then
+    if (input == "time") then
         --- Candidate(type, start, end, text, comment)
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
         yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
@@ -15,7 +15,7 @@ function date_translator(input, seg)
     -- @JiandanDream
     -- https://github.com/KyleBing/rime-wubi86-jidian/issues/54
 
-    if (input == "xq") then
+    if (input == "week") then
         local weakTab = {'日', '一', '二', '三', '四', '五', '六'}
         yield(Candidate("week", seg.start, seg._end, "周"..weakTab[tonumber(os.date("%w")+1)], ""))
         yield(Candidate("week", seg.start, seg._end, "星期"..weakTab[tonumber(os.date("%w")+1)], ""))
